68d5f8dae9834721d641eb6c605a121cf3b97a95
Everton Godoy <everton.mgodoy@gmail.com>
2024-09-03 16:55:46 -0300

    criada estrutura inicial

---
+++ src/main/java/sp/sanac/br/backend/Formulario/FormularioApplication.java
@@ -0,0 +1,13 @@
+package sp.sanac.br.backend.Formulario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormularioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FormularioApplication.class, args);
+	}
+
+}
